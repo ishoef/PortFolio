81a5afb178842fe3df6f5a2c0d5ad4145ac645ad
ishoef <ismailhossennayeb@gmail.com>
2025-09-03 23:27:49 +0600

    feat: add @vercel/analytics dependency and integrate it into MainLayout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router';
-import Header from '../Components/Header/Header';
-import Footer from '../Components/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const MainLayout = () => {
-    return (
-      <>
-        <Header></Header>
-        <div className='min-h-[calc(100vh-300px)]'>
-          <Outlet></Outlet>
-        </div>
-        <Footer></Footer>
-      </>
-    );
+  return (
+    <>
+      <Header></Header>
+      <div className="min-h-[calc(100vh-300px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+      <Analytics />
+    </>
+  );
 };
 
 export default MainLayout;
