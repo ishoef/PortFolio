ad6263891b58369e0870776f1d0c96208b3a5368
ishoef <ismailhossennayeb@gmail.com>
2025-09-03 23:56:48 +0600

    fix: some mistakes

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const MainLayout = () => {
   return (
